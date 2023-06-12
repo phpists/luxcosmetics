71c414d258da93607b1f9938e76a34e36723fce4
Ivan <ddafihh@gmail.com>
2023-06-12 12:44:41 +0300

    news_update

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,21 +3,42 @@
 @section('content')
 
 <br>
+
 <div class="container">
-    <div style="text-align: center;">
-        <div>
-            <h2>{{ $item->title }}</h2>
-        </div>
-        <div class="article__image">
-            <a href=""><img src="{{asset('images/uploads/news/' . $item->image)}}" alt="" style="width: 700px; margin: 0 auto;"></a>
-            <div class="article__date"><svg class="icon"><use xlink:href="{{asset('images/dist/sprite.svg#calendar')}}"></use></svg>{{$item->published_at}}</div>
-        </div>
-        <div>
-            <div class="article__intro">
-                <h3>{{ Str::limit(strip_tags($item->text), $limit = 30, $end = '...') }}</h3>
+    <div class="align-items-center>
+        <div class="col-md-6">
+            <div class="d-flex justify-content-center">
+                <div class="article__image">
+                    <a><img src="{{asset('images/uploads/news/' . $item->image)}}" alt="" style="width: 700px;"></a>
+                    <div class="article__date">
+                        <svg class="icon">
+                            <use xlink:href="{{asset('images/dist/sprite.svg#calendar')}}"></use>
+                        </svg>
+                        {{ \Carbon\Carbon::parse($item->published_at)->locale('ru')->format('d F Y') }}
+                    </div>
+                    <div class="text-center">
+                        <h2>{{ $item->title }}</h2>
+                    </div>
+                    <div>
+                        <h3>{{ Str::limit(strip_tags($item->text)) }}</h3>
+                    </div>
+                </div>
             </div>
         </div>
+            
+        </div>
     </div>
-    
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
