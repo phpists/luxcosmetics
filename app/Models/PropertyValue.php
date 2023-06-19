2544b37faf1ec7d49b38a713c27e2dbf758ff406
peekle86 <vitalik.gw@ukr.net>
2023-06-19 13:37:51 +0300

    catalog > filter > property values > sort

--- app/Models/PropertyValue.php
+++ app/Models/PropertyValue.php
@@ -18,7 +18,7 @@ class PropertyValue extends Model
 
     public function property()
     {
-        return $this->belongsTo(Property::class);
+        return $this->belongsTo(Property::class)->orderBy('value');
     }
 
 }
