f81f314ebf69572fbfb892551ba236746b9f95d8
peekle86 <vitalik.gw@ukr.net>
2023-10-24 23:19:02 +0300

    fixes

--- resources/views/emails/admin/order_created.blade.php
+++ resources/views/emails/admin/order_created.blade.php
@@ -2,6 +2,5 @@
     <h1 style="text-align: center">
         Новый заказ №{{ $order->id }}
     </h1>
-
     <x-mail::button :url="route('admin.orders.show', $order->id)">Просмотр</x-mail::button>
 </x-mail::message>
