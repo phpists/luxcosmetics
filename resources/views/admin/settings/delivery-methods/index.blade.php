6e3b946a217fc5899ad010cf0efd5bbae0cc0ecf
peekle86 <vitalik.gw@ukr.net>
2024-01-25 16:31:41 +0200

    lil fix

--- resources/views/admin/settings/delivery-methods/index.blade.php
+++ resources/views/admin/settings/delivery-methods/index.blade.php
@@ -6,7 +6,7 @@
             <!--begin::Page Heading-->
             <div class="d-flex align-items-baseline flex-wrap mr-5">
                 <!--begin::Page Title-->
-                <h5 class="text-dark font-weight-bold my-1 mr-5">Способі доставки</h5>
+                <h5 class="text-dark font-weight-bold my-1 mr-5">Способы доставки</h5>
                 <!--end::Page Title-->
             </div>
             <!--end::Page Heading-->
