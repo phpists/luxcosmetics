64be68d3aa18fc44f16734be68ce60785637bbb1
Mikhail Gaiday <michailgos@gmail.com>
2024-02-13 15:33:19 +0500

    fix js

--- public/js/app.js
+++ public/js/app.js
@@ -739,3 +739,19 @@ document.addEventListener('DOMContentLoaded', () => {
 })
  
 
+$(document).ready(function() {
+	$('.rulesmenu a[href^="#"]').on('click', function(e) {
+	    e.preventDefault();
+    
+	    var target = this.hash;
+	    var $target = $(target);
+    
+	    // Плавная прокрутка к элементу
+	    $('html, body').stop().animate({
+		'scrollTop': $target.offset().top
+	    }, 900, 'swing', function() {
+		window.location.hash = target;
+	    });
+	});
+    });
+    
