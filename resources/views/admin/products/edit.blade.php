4d84d89951c64cf77afa8c436adce44c00af2812
peekle86 <vitalik.gw@ukr.net>
2024-03-21 13:23:25 +0200

    fixes

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -303,25 +303,25 @@
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="lengthField">Длина:</label>
-                                            <input type="number" class="form-control" id="lengthField" name="length_product" min="0" max="100" step="1" value="{{ $product->length_product }}">
+                                            <input type="number" class="form-control" id="lengthField" name="length_product" value="{{ $product->length_product }}">
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="widthField">Ширина:</label>
-                                            <input type="number" class="form-control" id="widthField" name="width_product" min="0" max="100" step="1" value="{{ $product->width_product }}">
+                                            <input type="number" class="form-control" id="widthField" name="width_product" value="{{ $product->width_product }}">
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="heightField">Высота:</label>
-                                            <input type="number" class="form-control" id="heightField" name="height_product" min="0" max="100" step="1" value="{{ $product->height_product }}">
+                                            <input type="number" class="form-control" id="heightField" name="height_product" value="{{ $product->height_product }}">
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="weightField">Вес:</label>
-                                            <input type="number" class="form-control" id="weightField" name="weight_product" min="0" max="100" step="1" value="{{ $product->weight_product }}">
+                                            <input type="number" class="form-control" id="weightField" name="weight_product" value="{{ $product->weight_product }}">
                                         </div>
                                     </div>
                                     <div class="col-md-2">
@@ -356,7 +356,7 @@
                                     <div class="col-md-2">
                                         <div class="form-group">
                                             <label for="weightField">Количество спирта в %:</label>
-                                            <input type="number" class="form-control" id="spyrt" name="spyrt" min="0" max="100" step="1" value="{{ $product->spyrt }}">
+                                            <input type="number" class="form-control" id="spyrt" name="spyrt" value="{{ $product->spyrt }}">
                                         </div>
                                     </div>
                                 </div>
