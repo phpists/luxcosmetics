ab8aba88efbac3e8f072ffa14e1f95e41d0fd35d
peekle86 <vitalik.gw@gmail.com>
2024-07-31 10:41:16 +0300

    feedback tickets fix

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\TicketAnsweredMail;
+use App\Mail\TicketCreatedMail;
 use App\Models\FeedbackChat;
 use App\Models\FeedbackMessage;
 use App\Models\FeedbackMessageFile;
@@ -50,6 +52,8 @@ class FeedbackController extends Controller
             }
         }
 
+        \Mail::to($request->user()->email)->send(new TicketCreatedMail($feedback_chat));
+
         DB::commit();
 
         return redirect()->back()->with('success', 'Тикет успешно создан');
@@ -65,11 +69,15 @@ class FeedbackController extends Controller
 
     public function store_message(Request $request) {
         $data = $request->all();
+
+        $chat = FeedbackChat::find($data['chat_id']);
+
         $data['user_id'] = $request->user()->id;
         $message = new FeedbackMessage($data);
         $is_saved = $message->save();
         if ($request->ajax()) {
             if ($is_saved) {
+                \Mail::to($chat->user->email)->send(new TicketAnsweredMail($chat));
                 return response()->json([
                     'status' => true
                 ]);
@@ -82,6 +90,7 @@ class FeedbackController extends Controller
         }
         else {
             if ($is_saved) {
+                \Mail::to($chat->user->email)->send(new TicketAnsweredMail($chat));
                 return redirect()->back()->with('success', 'Успешно отправлено');
             } else {
                 return redirect()->back()->with('error', 'Не удалось отправить сообщение');
