50cb852906892f3e6f727532a95bdc5b2fd41f4d
peekle86 <vitalik.gw@gmail.com>
2024-09-10 12:07:54 +0300

    lil fix

--- resources/views/catalog/banners/horizontal_img.blade.php
+++ resources/views/catalog/banners/horizontal_img.blade.php
@@ -1,5 +1,11 @@
 <div class="category-page__banner">
-    <img src="{{ $catalogBanner->getImgSrc($catalogBanner->data['img_960']) }}" alt="" class="for-desktop">
-    <img src="{{ $catalogBanner->getImgSrc($catalogBanner->data['img_768']) }}" alt="" class="for-tablet">
-    <img src="{{ $catalogBanner->getImgSrc($catalogBanner->data['img_375']) }}" alt="" class="for-mobile">
+    @isset($catalogBanner->data['link'])
+        <a href="{{ $catalogBanner->data['link'] }}">
+            @endif
+            <img src="{{ $catalogBanner->getImgSrc($catalogBanner->data['img_960']) }}" alt="" class="for-desktop">
+            <img src="{{ $catalogBanner->getImgSrc($catalogBanner->data['img_768']) }}" alt="" class="for-tablet">
+            <img src="{{ $catalogBanner->getImgSrc($catalogBanner->data['img_375']) }}" alt="" class="for-mobile">
+            @isset($catalogBanner->data['link'])
+        </a>
+    @endisset
 </div>
