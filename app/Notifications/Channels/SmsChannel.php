58713ab9435680e4151fce17748e2abf099de82b
peekle86 <vitalik.gw@gmail.com>
2024-10-25 14:12:09 +0300

    mock phone sending

--- app/Notifications/Channels/SmsChannel.php
+++ app/Notifications/Channels/SmsChannel.php
@@ -12,7 +12,21 @@ class SmsChannel
     public function send(object $notifiable, Notification $notification): void
     {
         $message = $notification->toSms($notifiable);
-        $message->to($notifiable->routeNotificationFor(self::class));
-        $message->send();
+        $phone = $notifiable->routeNotificationFor(self::class);
+        $message->to($phone);
+
+        if (config('app.env') == 'production')
+            $message->send();
+
+        if (config('app.env') == 'staging' && !$this->isMockPhone($phone))
+            $message->send();
+    }
+
+    private function isMockPhone(string $phone): bool
+    {
+        $phone = preg_replace('/\D/', '', $phone);
+        $mockPhone = env('SMS_MOCK_PHONE');
+
+        return $phone == $mockPhone;
     }
 }
