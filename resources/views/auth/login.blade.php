da503314a41d999a76bef8d71e865137516c02ac
peekle86 <vitalik.gw@gmail.com>
2024-10-25 13:43:50 +0300

    gcaptcha for otp login | disabled btns styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,82 +1,9 @@
-{{--@extends('layouts.app')--}}
-
-{{--@section('content')--}}
-{{--<div class="container">--}}
-{{--    <div class="row justify-content-center">--}}
-{{--        <div class="col-md-8">--}}
-{{--            <div class="card">--}}
-{{--                <div class="card-header">{{ __('Login') }}</div>--}}
-
-{{--                <div class="card-body">--}}
-{{--                    <form method="POST" action="{{ route('login') }}">--}}
-{{--                        @csrf--}}
-
-{{--                        <div class="row mb-3">--}}
-{{--                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
-
-{{--                                @error('email')--}}
-{{--                                    <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="row mb-3">--}}
-{{--                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">--}}
-
-{{--                                @error('password')--}}
-{{--                                    <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="row mb-3">--}}
-{{--                            <div class="col-md-6 offset-md-4">--}}
-{{--                                <div class="form-check">--}}
-{{--                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
-
-{{--                                    <label class="form-check-label" for="remember">--}}
-{{--                                        {{ __('Remember Me') }}--}}
-{{--                                    </label>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="row mb-0">--}}
-{{--                            <div class="col-md-8 offset-md-4">--}}
-{{--                                <button type="submit" class="btn btn-primary">--}}
-{{--                                    {{ __('Login') }}--}}
-{{--                                </button>--}}
-{{--                                <button type="submit" class="btn btn-primary">--}}
-{{--                                    <a href="{{ route('register') }}">--}}
-{{--                                    {{ __('Register') }}--}}
-{{--                                    </a>--}}
-{{--                                </button>--}}
-{{--                                @if (Route::has('password.request'))--}}
-{{--                                    <a class="btn btn-link" href="{{ route('password.request') }}">--}}
-{{--                                        {{ __('Forgot Your Password?') }}--}}
-{{--                                    </a>--}}
-{{--                                @endif--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </form>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
-{{--@endsection--}}
-
 @extends('layouts.app')
+
+@section('title', $metaTitle = getSeoTemplateTitle(\App\Enums\SeoTemplateEnum::REGISTER))
+@section('description', $metaDescription = getSeoTemplateDescription(\App\Enums\SeoTemplateEnum::REGISTER))
+@section('og:title', $metaTitle)
+@section('og:description', $metaDescription)
 
 @section('content')
     <section class="signin-page">
@@ -110,6 +37,8 @@
 @endsection
 
 @section('scripts')
+    <script
+        src="https://www.google.com/recaptcha/api.js?render={{ config('services.google.captcha.site_key') }}"></script>
     <script src="{{ asset('js/inputmask/inputmask.js') }}"></script>
     <script src="{{ asset('js/inputmask/jquery.inputmask.js') }}"></script>
     <script type="text/javascript">
@@ -119,14 +48,23 @@
             $(document).on('submit', '#loginForm', function (e) {
                 e.preventDefault();
                 const $form = $(this);
-                submitLiveForm($form, function (response, json) {
-                    if (response.status == 200) {
-                        $('#otpForm').find('[name="phone"]').val($form.find('[name="phone"]').val())
-                        openModal('#otpModal')
-                        if (json.message)
-                            $('button:submit[form="resendCodeForm"]').setCooldown(60);
-                    }
-                })
+
+                $form.find('button:submit').prop('disabled', true)
+
+                grecaptcha.ready(function () {
+                    grecaptcha.execute('{{ config('services.google.captcha.site_key') }}', {action: 'submit'})
+                        .then(function (token) {
+                            submitLiveForm($form, function (response, json) {
+                                if (response.status == 200) {
+                                    $('#otpForm').find('[name="phone"]').val($form.find('[name="phone"]').val())
+                                    openModal('#otpModal')
+                                    if (json.message)
+                                        $('button:submit[form="resendCodeForm"]').setCooldown(60);
+                                }
+                                $form.find('button:submit').prop('disabled', false)
+                            })
+                        });
+                });
             })
         });
     </script>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,8 @@
                     <p><b>Создайте учетную запись, чтобы пользоваться эксклюзивными преимуществами</b></p>
                     <p>Включая отслеживание заказов и ранний доступ к нашим мероприятиям и специальным предложениям
                     </p>
-                    <form id="registerForm" method="POST" action="{{ route('register.store') }}" class="form form--box  typography">
+                    <form id="registerForm" method="POST" action="{{ route('register.store') }}"
+                          class="form form--box  typography">
                         @csrf
                         @if ($errors->any())
                             <div class="alert alert-danger">
@@ -30,13 +31,13 @@
                         <div class="form__row">
                             <div class="form__col form__col--50">
                                 <div class="form__fieldset">
-                                    <legend class="form__label">Имя  *</legend>
-                                    <input name="name" required type="text" class="form__input" >
+                                    <legend class="form__label">Имя *</legend>
+                                    <input name="name" required type="text" class="form__input">
                                 </div>
                             </div>
                             <div class="form__col form__col--50">
                                 <div class="form__fieldset">
-                                    <legend class="form__label">Фамилия  *</legend>
+                                    <legend class="form__label">Фамилия *</legend>
                                     <input name="surname" required type="text" class="form__input">
                                 </div>
                             </div>
@@ -45,7 +46,8 @@
                             <div class="form__col form__col--50">
                                 <div class="form__fieldset">
                                     <legend class="form__label">Телефон *</legend>
-                                    <input type="tel" required name="phone" id="phone_inp" class="form__input phone_inp">
+                                    <input type="tel" required name="phone" id="phone_inp"
+                                           class="form__input phone_inp">
                                 </div>
                             </div>
                             <div class="form__col form__col--50">
@@ -58,7 +60,8 @@
                         <div class="form__fieldset">
                             <legend class="form__label">Дата рождения</legend>
                             <div class="form__row">
-                                <div class="form__col form__col--33"><input type="number" max="31" min="1" name="day" class="form__input" value="10">
+                                <div class="form__col form__col--33"><input type="number" max="31" min="1" name="day"
+                                                                            class="form__input" value="10">
                                 </div>
                                 <div class="form__col form__col--33">
                                     <select class="form__input" name="month" style="background: white">
@@ -77,25 +80,30 @@
                                     </select>
                                 </div>
                                 <div class="form__col form__col--33">
-                                    <input type="number" name="year" class="form__input" min="1900" value='{{date("Y")}}' max="{{date("Y")}}">
+                                    <input type="number" name="year" class="form__input" min="1900"
+                                           value='{{date("Y")}}' max="{{date("Y")}}">
                                 </div>
                             </div>
                         </div>
                         <div class="form__fieldset">
                             <label class="checkbox">
-                                <input type="checkbox" name="newsletter" />
-                                <div class="checkbox__text">Подпишитесь на рассылку новостей<br> <small>Установив этот флажок, вы соглашаетесь получать сообщения о запуске новых продуктов, эксклюзивных событиях в магазине и сезонных рекламных акциях.</small></div>
+                                <input type="checkbox" name="newsletter"/>
+                                <div class="checkbox__text">Подпишитесь на рассылку новостей<br> <small>Установив этот
+                                        флажок, вы соглашаетесь получать сообщения о запуске новых продуктов,
+                                        эксклюзивных событиях в магазине и сезонных рекламных акциях.</small></div>
                             </label>
                         </div>
                         <div class="form__fieldset">
                             <label class="checkbox">
                                 <input type="checkbox" name="agreement" value="1" required/>
-                                <div class="checkbox__text">Я прочитал <a href="/pages/policy">Условия и положения</a> , а также <a href="/pages/policy">Политику конфиденциальности</a> и согласен с ними*</div>
+                                <div class="checkbox__text">Я прочитал <a href="/pages/policy">Условия и положения</a> ,
+                                    а также <a href="/pages/policy">Политику конфиденциальности</a> и согласен с ними*
+                                </div>
                             </label>
                         </div>
 
 
-                        <button id="reg_btn" class="btn btn--accent">Зарегистрироваться</button>
+                        <button type="submit" id="reg_btn" class="btn btn--accent">Зарегистрироваться</button>
                     </form>
 
                 </div>
@@ -105,29 +113,34 @@
 @endsection
 
 @section('scripts')
+    <script
+        src="https://www.google.com/recaptcha/api.js?render={{ config('services.google.captcha.site_key') }}"></script>
     <script src="{{ asset('js/inputmask/inputmask.js') }}"></script>
     <script src="{{ asset('js/inputmask/jquery.inputmask.js') }}"></script>
     <script type="text/javascript">
-        $(document).ready(function(){
+        $(document).ready(function () {
             Inputmask("+7 (999) 999-99-99").mask('.phone_inp');
 
             $(document).on('submit', '#registerForm', function (e) {
                 e.preventDefault();
                 const $form = $(this);
-                submitLiveForm($form, function (response, json) {
-                    if (response.status == 200) {
-                        $('#otpForm').find('[name="phone"]').val($form.find('[name="phone"]').val())
-                        openModal('#otpModal')
-                        if (json.message)
-                            $('button:submit[form="resendCodeForm"]').setCooldown(60);
-                    }
-                })
-            })
 
-            $(document).on('submit', '#otpForm', function (e) {
-                e.preventDefault();
-                const $form = $(this);
-                submitLiveForm($form)
+                $form.find('button:submit').prop('disabled', true)
+
+                grecaptcha.ready(function () {
+                    grecaptcha.execute('{{ config('services.google.captcha.site_key') }}', {action: 'submit'})
+                        .then(function (token) {
+                            submitLiveForm($form, function (response, json) {
+                                if (response.status == 200) {
+                                    $('#otpForm').find('[name="phone"]').val($form.find('[name="phone"]').val())
+                                    openModal('#otpModal')
+                                    if (json.message)
+                                        $('button:submit[form="resendCodeForm"]').setCooldown(60);
+                                }
+                                $form.find('button:submit').prop('disabled', false)
+                            })
+                        });
+                });
             })
         });
     </script>
