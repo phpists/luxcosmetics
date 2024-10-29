163a3509b4319e317d7171cd0e147555459167eb
peekle86 <vitalik.gw@gmail.com>
2024-10-29 13:28:20 +0200

    lil fix

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -78,12 +78,6 @@
                                 </div>
                             @endforeach
                         @endif
-                        @if (session('status'))
-                            <div class="alert alert-primary" role="alert">
-                                Мы отправили ссылку для сброса пароля <br>
-                                по электронной почте!
-                            </div>
-                        @endif
                         @if(session()->get('success'))
                             <div class="alert alert-primary" role="alert">
                                 {{ session()->get('success') }}
@@ -104,9 +98,6 @@
                         <div class="form-group">
                             <div class="d-flex justify-content-between mt-n5">
                                 <label class="font-size-h6 font-weight-bolder text-dark pt-5">Пароль</label>
-                                <a href="javascript:;"
-                                   class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5"
-                                   id="kt_login_forgot">Забыли пароль?</a>
                             </div>
                             <input class="form-control form-control-solid h-auto py-6 px-6 rounded-lg" name="password"
                                    type="password" autocomplete="off"/>
@@ -123,103 +114,6 @@
                     <!--end::Form-->
                 </div>
                 <!--end::Signin-->
-                <!--begin::Signup-->
-                <div class="login-form login-signup">
-                    <!--begin::Form-->
-                    <form class="form" novalidate="novalidate" id="kt_login_signup_form">
-                        <!--begin::Title-->
-                        <div class="pb-13 pt-lg-0 pt-5">
-                            <h3 class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg">Sign Up</h3>
-                            <p class="text-muted font-weight-bold font-size-h4">Enter your details to create your
-                                account</p>
-                        </div>
-                        <!--end::Title-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <input class="form-control form-control-solid h-auto py-6 px-6 rounded-lg font-size-h6"
-                                   type="text" placeholder="Fullname" name="fullname" autocomplete="off"/>
-                        </div>
-                        <!--end::Form group-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <input class="form-control form-control-solid h-auto py-6 px-6 rounded-lg font-size-h6"
-                                   type="email" placeholder="Email" name="email" autocomplete="off"/>
-                        </div>
-                        <!--end::Form group-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <input class="form-control form-control-solid h-auto py-6 px-6 rounded-lg font-size-h6"
-                                   type="password" placeholder="Password" name="password" autocomplete="off"/>
-                        </div>
-                        <!--end::Form group-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <input class="form-control form-control-solid h-auto py-6 px-6 rounded-lg font-size-h6"
-                                   type="password" placeholder="Confirm password" name="cpassword" autocomplete="off"/>
-                        </div>
-                        <!--end::Form group-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <label class="checkbox mb-0">
-                                <input type="checkbox" name="agree"/>
-                                <span></span>
-                                <div class="ml-2">I Agree the
-                                    <a href="#">terms and conditions</a>.
-                                </div>
-                            </label>
-                        </div>
-                        <!--end::Form group-->
-                        <!--begin::Form group-->
-                        <div class="form-group d-flex flex-wrap pb-lg-0 pb-3">
-                            <button type="button" id="kt_login_signup_submit"
-                                    class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-4">Submit
-                            </button>
-                            <button type="button" id="kt_login_signup_cancel"
-                                    class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3">Cancel
-                            </button>
-                        </div>
-                        <!--end::Form group-->
-                    </form>
-                    <!--end::Form-->
-                </div>
-                <!--end::Signup-->
-                <!--begin::Forgot-->
-                <div class="login-form login-forgot">
-                    <!--begin::Form-->
-                    <form class="form" novalidate="novalidate" id="kt_login_forgot_form" method="POST"
-                          action="{{ route('password.email') }}">
-                    @csrf
-                    <!--begin::Title-->
-                        <div class="pb-13 pt-lg-0 pt-5">
-                            <h3 class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg">Забыли пароль?</h3>
-                            <p class="text-muted font-weight-bold font-size-h4">Введите адрес электронной почты, чтобы
-                                сбросить пароль</p>
-                        </div>
-                        <!--end::Title-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <input class="form-control form-control-solid h-auto py-6 px-6 rounded-lg font-size-h6"
-                                   type="email" placeholder="Email" name="email" autocomplete="off"
-                                   value="{{ old('email') }}"/>
-                        </div>
-
-                        <!--end::Form group-->
-                        <!--begin::Form group-->
-                        <div class="form-group d-flex flex-wrap pb-lg-0">
-                            <button type="submit" id="kt_login_forgot_submit"
-                                    class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-4">
-                                Отправить
-                            </button>
-                            <button type="button" id="kt_login_forgot_cancel"
-                                    class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3">
-                                Отменить
-                            </button>
-                        </div>
-                        <!--end::Form group-->
-                    </form>
-                    <!--end::Form-->
-                </div>
-                <!--end::Forgot-->
             </div>
             <!--end::Content body-->
             <!--begin::Content footer-->
