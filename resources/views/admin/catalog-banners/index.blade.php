6a07ac102a6e19e75071e41e5554c298bd43844e
peekle86 <vitalik.gw@gmail.com>
2024-11-14 13:56:28 +0200

    filepond local

--- resources/views/admin/catalog-banners/index.blade.php
+++ resources/views/admin/catalog-banners/index.blade.php
@@ -120,8 +120,8 @@
 @endsection
 
 @section('js_after')
-    <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
-    <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+    <script src="{{ asset('super_admin/js/filepond/filepond-plugin-image-preview.js') }}"></script>
+    <script src="{{ asset('super_admin/js/filepond/filepond.js') }}"></script>
     <script>
 
         $(function () {
